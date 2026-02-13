c225b6e7452c4266e5411a1f8248f5886a3dd46e
Matthew Starks <ukandit@hotmail.com>
2026-02-13 10:06:40 -0500

    Create sessionSet.js

---
+++ src/Constructors/sessionSet.js
@@ -0,0 +1,26 @@
+class SetObject {
+  constructor(weight = 0, reps, time = 0, type, activeTime = False) {
+    this.weight = weight;
+    this.reps = reps;
+    this.time = time;
+    this.timeType = activeTime;
+  }
+  setWeight(newWeight) {
+    this.weight = newWeight;
+  }
+  setReps(newReps) {
+    this.reps = newReps;
+  }
+  setTime(newTime) {
+    this.setTime = newTime;
+  }
+  getTime() {
+    return this.time;
+  }
+  getWeight() {
+    return this.weight;
+  }
+  getReps() {
+    return this.reps;
+  }
+}
